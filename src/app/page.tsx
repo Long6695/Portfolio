d12a7c4192e5396dd1951ceef3f6eceffa692635
Long6695 <thaihothanhlong95@gmail.com>
2023-04-05 17:11:13 +0700

    feat: navbar layout & scroll section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,15 @@
 "use client";
-import ContactSection from "@/components/contact";
-import AboutSection from "@/components/about";
-import WorksSection from "@/components/works";
-import React, { useRef, useState } from "react";
-import Nav, { MenuType } from "@/components/nav";
+import React from "react";
 import ThemeContextProvider from "@/context/theme-context";
+import ScrollContextProvider from "@/context/scroll-context";
+import PortfolioPage from "@/components";
 
 const MainPage = () => {
-  const aboutSection = useRef<HTMLDivElement>(null);
-  const projectSection = useRef<HTMLDivElement>(null);
-  const contactSection = useRef<HTMLDivElement>(null);
-  const [active, setActive] = useState(1);
-
-  const sectionRefMap = {
-    1: aboutSection,
-    2: projectSection,
-    3: contactSection,
-  };
-
-  const scrollToView = (num: keyof typeof sectionRefMap) => {
-    sectionRefMap[num]?.current?.scrollIntoView({ behavior: "smooth" });
-  };
-
-  const handleScroll = (item: MenuType) => {
-    scrollToView(item.id as keyof typeof sectionRefMap);
-    setActive(item.id);
-  };
-
   return (
     <ThemeContextProvider>
-      <Nav onScroll={handleScroll} active={active} />
-      <div className="section-layout px-7 pt-12" ref={aboutSection}>
-        <AboutSection />
-      </div>
-      <div className="section-layout px-7 pt-12" ref={projectSection}>
-        <WorksSection />
-      </div>
-      <div className="section-layout px-7 pt-12" ref={contactSection}>
-        <ContactSection />
-      </div>
+      <ScrollContextProvider>
+        <PortfolioPage />
+      </ScrollContextProvider>
     </ThemeContextProvider>
   );
 };
