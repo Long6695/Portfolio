f229a216923b7296e8cf289634a009a873931e1c
Long6695 <thaihothanhlong95@gmail.com>
2023-04-11 22:02:18 +0700

    feat/parallax-scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ import React from "react";
 import ThemeContextProvider from "@/context/theme-context";
 import ScrollContextProvider from "@/context/scroll-context";
 import PortfolioPage from "@/components";
+import {ParallaxProvider} from 'react-scroll-parallax';
 
 const MainPage = () => {
   return (
+    <ParallaxProvider>
     <ThemeContextProvider>
       <ScrollContextProvider>
         <PortfolioPage />
       </ScrollContextProvider>
     </ThemeContextProvider>
+    </ParallaxProvider>
   );
 };
 
